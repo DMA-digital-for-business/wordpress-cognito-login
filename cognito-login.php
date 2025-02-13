5c213db790a84aa61b96c4f092ad4403fb65dd88
Matteo Collina <matteo.collina@dma.it>
2025-02-13 14:46:39 +0100

    Incluso plugin Basic Auth https://github.com/WP-API/Basic-Auth

--- cognito-login.php
+++ cognito-login.php
@@ -3,7 +3,7 @@
   Plugin Name: Cognito Login
   Plugin URI: https://github.com/DMA-digital-for-business/wordpress-cognito-login
   description: WordPress plugin for integrating with Cognito for User Pools
-  Version: 1.7.1
+  Version: 1.7.2
   Author: DMA
   Author URI: https://www.dma.it/
 */
@@ -233,8 +233,64 @@ class Cognito_Login
     }
   }
 
+
+  static function json_basic_auth_handler( $user ) {
+    global $wp_json_basic_auth_error;
+  
+    $wp_json_basic_auth_error = null;
+  
+    // Don't authenticate twice
+    if ( ! empty( $user ) ) {
+      return $user;
+    }
+  
+    // Check that we're trying to authenticate
+    if ( !isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+      return $user;
+    }
+  
+    $username = $_SERVER['PHP_AUTH_USER'];
+    $password = $_SERVER['PHP_AUTH_PW'];
+  
+    /**
+     * In multi-site, wp_authenticate_spam_check filter is run on authentication. This filter calls
+     * get_currentuserinfo which in turn calls the determine_current_user filter. This leads to infinite
+     * recursion and a stack overflow unless the current function is removed from the determine_current_user
+     * filter during authentication.
+     */
+    remove_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
+  
+    $user = wp_authenticate( $username, $password );
+  
+    add_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
+  
+    if ( is_wp_error( $user ) ) {
+      $wp_json_basic_auth_error = $user;
+      return null;
+    }
+  
+    $wp_json_basic_auth_error = true;
+  
+    return $user->ID;
+  }
+  
+  static function json_basic_auth_error( $error ) {
+    // Passthrough other errors
+    if ( ! empty( $error ) ) {
+      return $error;
+    }
+  
+    global $wp_json_basic_auth_error;
+  
+    return $wp_json_basic_auth_error;
+  }
+
 }
 
+// Basic Auth
+add_filter( 'determine_current_user', array('Cognito_Login', 'json_basic_auth_handler') );
+
+add_filter( 'rest_authentication_errors', array('Cognito_Login', 'json_basic_auth_error') );
 
 
 // --- Add Shortcodes ---
